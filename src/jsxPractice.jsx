0627d333e8e8d9913181fc3638fd6ff185f0be01
Amirdhagadesan <amir.mca@gmail.com>
2023-08-13 10:26:19 -0700

    React Prerequisites

---
+++ src/jsxPractice.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>React Prerequisites</h1>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JS/ES6</li>
+      <li>NPM</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
